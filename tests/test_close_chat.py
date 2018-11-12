c6ac48716711b606fcaf4bf4eeb47bd21996ec32
Roman Melnychuk <jenikuse@gmail.com>
2018-11-13 02:03:31 +0300

    added cases for closing chat and verification that chat is not displaying

--- tests/test_close_chat.py
+++ tests/test_close_chat.py
@@ -1,9 +1,23 @@
 import unittest
 
+from selenium import webdriver
 
-class TestCloseChat(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+driver = webdriver.Firefox()
+
+
+class TestOpenChat(unittest.TestCase):
+
+    def test_01_close_chat(self):
+        btn_close = driver.find_element_by_css_selector("div.webim-action-close")
+        btn_close.click()
+
+    def test_02_check_if_chat_closed(self):
+        msg_area = driver.find_element_by_css_selector(".webim-message-area")
+        self.assertFalse(msg_area.is_displayed())
+
+    def test_10(self):
+        driver.close()
 
 
 if __name__ == '__main__':
