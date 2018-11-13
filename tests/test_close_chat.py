1e66cad69e2ddc19f290a9d64d8f082c3bc85f66
Roman Melnychuk <jenikuse@gmail.com>
2018-11-14 01:18:11 +0300

    added assert for close_chat case. Also added case with validation for minimizing chat window.

--- tests/test_close_chat.py
+++ tests/test_close_chat.py
@@ -10,13 +10,24 @@ class TestCloseChat(unittest.TestCase):
         self.assertTrue(btn_close.is_displayed(),
                         "chat close button is not displaying")
         btn_close.click()
+        self.assertFalse(btn_close.is_displayed(), 
+        "chat close button is displaying, but chat window has been closed")
 
-    def test_16_check_if_chat_closed(self):
+    def test_16_reopen_chat(self):
+        driver.find_element_by_css_selector(
+            ".webim_button > img:nth-child(1)").click()
         msg_area = driver.find_element_by_css_selector(".webim-dialogues")
-        self.assertFalse(msg_area.is_displayed(),
-                         "chat was not closed")
-        driver.close()
+        self.assertTrue(msg_area.is_displayed(), "chat is not displaying")
 
+    def test_17_minimize_chat(self):
+        btn_minimize = driver.find_element_by_css_selector(".webim-action-minimize")
+        self.assertTrue(btn_minimize.is_displayed(),
+                        "chat minimize button is not displaying")
+        btn_minimize.click()
+        self.assertFalse(btn_minimize.is_displayed(), "chat minimize button is \
+                        displaying, but chat window has been minimized")
+
+        driver.close()
 
 if __name__ == "__main__":
     unittest.main()
