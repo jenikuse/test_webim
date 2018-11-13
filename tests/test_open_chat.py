96b430f37035b37b4d27cd8fd5fcb4ee68637a61
Roman Melnychuk <jenikuse@gmail.com>
2018-11-13 16:21:30 +0300

    added asserts to the case of opening chat

--- tests/test_open_chat.py
+++ tests/test_open_chat.py
@@ -3,41 +3,31 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait as Wait
 
 
-
-url = 'https://demo-pro.webim.ru'
 driver = webdriver.Firefox()
-wait = Wait(driver, 30)
+url = "https://demo-pro.webim.ru"
 
 
 class TestOpenChat(unittest.TestCase):
 
     def test_01_open_webpage(self):
         driver.get(url)
+        self.assertIn("веб мессенджер", driver.title.lower())
         time.sleep(2)  # waiting for chat panel loading
 
-
     def test_02_open_chat(self):
-        chat_panel = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div/div/a[2]/img")
+        chat_panel = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[1]/div/div/div/div/a[2]/img")
+        self.assertTrue(chat_panel.is_displayed())
         chat_panel.click()
 
-
-    def test_03_check(self):
-
+    def test_03_check_that_chat_is_open(self):
         msg_welcome = driver.find_element_by_css_selector(
             "li.webim-message:nth-child(1) > div:nth-child(3) > span:nth-child(1)").text
-
         self.assertIn("добро пожаловать", msg_welcome.lower())
 
 
-    def test_10_close_driver(self):
-
-        driver.close()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 
