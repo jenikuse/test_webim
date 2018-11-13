2a3c2f96dbd4ac7c68c71d3462d52fc6a18047b8
Roman Melnychuk <jenikuse@gmail.com>
2018-11-13 23:47:57 +0300

    added TestSendMsg class to the test suite

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,15 +1,21 @@
 import unittest
 
-open_chat = unittest.TestLoader().loadTestsFromTestCase()
-send_msg = unittest.TestLoader().loadTestsFromTestCase()
-send_file = unittest.TestLoader().loadTestsFromTestCase()
-rate_operator = unittest.TestLoader().loadTestsFromTestCase()
-close_chat = unittest.TestLoader().loadTestsFromTestCase()
+from test_open_chat import TestOpenChat
+from test_send_message import TestSendMsg
+from test_send_file import TestSendFile
+from test_rate_operator import TestRateOperator
+from test_close_chat import TestCloseChat
 
 
-full_suite = [open_chat, send_msg, send_file, rate_operator, close_chat]
+open_chat = unittest.TestLoader().loadTestsFromTestCase(TestOpenChat)
+send_msg = unittest.TestLoader().loadTestsFromTestCase(TestSendMsg)
+# send_file = unittest.TestLoader().loadTestsFromTestCase(TestSendFile)
+# rate_operator = unittest.TestLoader().loadTestsFromTestCase(TestRateOperator)
+close_chat = unittest.TestLoader().loadTestsFromTestCase(TestCloseChat)
 
-test_suite = unittest.TestSuite()
+
+# test_suite = [open_chat, send_msg, send_file, rate_operator, close_chat]
+test_suite = unittest.TestSuite([open_chat, send_msg])
 
 
 unittest.TextTestRunner(verbosity=2).run(test_suite)
