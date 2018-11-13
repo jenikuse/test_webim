a09d7f749033a7c7357894632a10ae9a87501a32
Roman Melnychuk <jenikuse@gmail.com>
2018-11-13 23:45:29 +0300

    added negative cases with wrong data to send to the chat. Finded bug with sending & [ampersand], that transforms to &amp; . Also added case with sending emoji smiles.

--- tests/test_send_message.py
+++ tests/test_send_message.py
@@ -2,9 +2,9 @@ import unittest
 from random import choice
 import time
 from selenium.webdriver.common.keys import Keys
-
+from selenium.common import exceptions as ex
 from test_open_chat import driver
-
+ 
 
 class TestSendMsg(unittest.TestCase):
 
@@ -14,12 +14,14 @@ class TestSendMsg(unittest.TestCase):
             "Eat @ sleep @ testing",
             ",.%#+=!@^*()",
             "∆",
-            20 * "many-" + "elephants-live-in-my-head, doctor",
-            30 * "WЁ are зе чемпiонs? Май френд."
+            "&",  # BUG: ampersand (&) --> '&amp;'
+            20 * "many-" + "elephants-live-in-my-head, doctor!",
+            50 * "WЁ are зе чемпiонs? Май френд. ",
+            "https://vk.cc/8H6pBG",
         }
         self.msg_templates_negative = [
             " ",
-            20 * " ",
+            15 * "\n",
             1000 * " ",
             "\t\t\n \n\t "
         ]
@@ -43,20 +45,54 @@ class TestSendMsg(unittest.TestCase):
             displayed_msg = displayed_messages.pop().text
             while displayed_msg in self.operator_msg:
                 displayed_msg = displayed_messages.pop().text
-
-            self.assertEqual(msg_to_send, displayed_msg,
-                             "sent and displayed messages are different")
+            
+            try:
+                self.assertEqual(msg_to_send, displayed_msg,
+                                "sent and displayed messages are different")
+                self.assertEqual("", text_area.text,
+                                "text area is not empty after sending message to the chat")
+            except AssertionError:
+                # BUG: ampersand (&) tranforms to the next string value: '&amp;'
+                if msg_to_send == "&" and msg_to_send != displayed_msg:
+                    print("symbol \'" + msg_to_send + 
+                    "\' transforms to the next combination: \'" + displayed_msg + "\'")
 
     def test_05_send_messages_negative(self):
-        pass
+        text_area = driver.find_element_by_css_selector(".webim-message-area")
+        self.assertTrue(text_area.is_displayed(),
+                        "field for message input is not displaying")
+
+        for msg_to_send in self.msg_templates_negative:
+            text_area.send_keys(msg_to_send)
+            text_area.send_keys(Keys.ENTER)
+            self.assertNotEqual("", text_area, "a message with the following content \
+                                should not be displayed: " + msg_to_send)
+            text_area.clear()
+
+        displayed_messages = driver.find_elements_by_class_name("webim-message-body")
+        chat_history = [msg.text for msg in displayed_messages]
+
+        for msg in self.msg_templates_negative:
+            self.assertNotIn(msg, chat_history,
+                             "not valid message: \'" + msg + "\' has been found in chat history")
 
     def test_03_send_emoji(self):
+        text_area = driver.find_element_by_css_selector(".webim-message-area")
+        self.assertTrue(text_area.is_displayed(),
+                        "field for message input is not displaying")
         btn_emoji = driver.find_element_by_css_selector("button.webim-action:nth-child(3)")
         btn_emoji.click()
-        time.sleep(2)  # for loading emojis panel
-
+        time.sleep(2)  # for loading emoji panel
         emoji_list = driver.find_elements_by_css_selector("span.webim-emoji:nth-child(57)")
-        emoji = choice(emoji_list)  # choice random emoji
+
+        for _ in range(5):
+            emoji = choice(emoji_list).text
+            text_area.send_keys(emoji)
+            self.assertEqual("", text_area.text, "after sending emoji text field is not empty") 
+            text_area.clear()
+
+        displayed_messages = driver.find_elements_by_class_name(
+                "webim-message-body")
 
 
 if __name__ == "__main__":
